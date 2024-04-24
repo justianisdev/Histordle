097fb0cc72122712b740d3c21cba4a58507f5959
underhscore <150970555+underhscore@users.noreply.github.com>
2024-04-24 14:28:07 -0400

    Update imagle.js

--- Imagle/imagle.js
+++ Imagle/imagle.js
@@ -12,7 +12,7 @@ let inputbox6 = document.querySelector('#box6')
 let submit = document.querySelector('#subbox');
 let textbox = document.querySelector('#textbox');
 let change = document.querySelector('#change')
-let answer = "luddites"
+let answer = "no mans land"
 let count = 0
 const jsConfetti = new JSConfetti()
 
@@ -36,7 +36,7 @@ function correct(){
  });
 }
 function wrong(){
- change.textContent = "You Lost, Answer: Luddites"
+ change.textContent = "You Lost, Answer: No Mans Land"
  change.style.color = "red"
  textbox.disabled = true
  submit.disabled = true
@@ -51,15 +51,15 @@ if (count == 2){
 }
 if (count == 3){
  inputbox3.textContent = textbox.value
-  window.alert('Many of these people where often killed for protesting')
+  window.alert('')
 }
 if (count == 4){
  inputbox4.textContent = textbox.value
-  window.alert('effective moment (engish movement)')
+  window.alert('')
 }
 if (count == 5){
  inputbox5.textContent = textbox.value
-  window.alert('anti-technological advancements (happend during 17th century)')
+  window.alert('')
 }
 if (count == 6){
  inputbox6.textContent = textbox.value
